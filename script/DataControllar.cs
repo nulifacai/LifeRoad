e00c6051ebda94ae50a81b876c84e2df2bf48b86
hzh <624424705@qq.com>
2019-07-06 16:26:25 +0800

    数据层

---
+++ script/DataControllar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+using System.Text;
+
+public class DataControllar : MonoBehaviour {
+
+    public static DataControllar instance;
+
+    string path;    //文字滚屏txt地址
+    StreamReader sr;//数据流对象
+    List<string> word;//用于存储文字滚屏文字的队列
+    WinnerCondition wc;//存储游戏数据对象
+
+    private void Start()
+    {
+        InIt();
+        getData();
+    }
+
+    /*
+     *初始化操作
+     */
+    public void InIt()
+    {
+        path = Application.dataPath + "/test1.txt";
+        sr = new StreamReader(path);
+        word = new List<string>();
+        wc = new WinnerCondition();
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    /*
+    **获取UI目录节点
+    */
+    public Transform getUIRoot()
+    {
+        return GameObject.Find("UIRoot").transform;
+    }
+
+    /*
+    *获取文字滚屏队列
+    */
+    public List<string> getData()
+    {
+        string result = sr.ReadToEnd();
+        string[] data = result.Split('\n');
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            //Debug.Log(data[i]);
+            word.Add(data[i]);
+        }
+
+        return word;
+    }
+
+
+
+}
---
+++ script/WinnerCondition.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerCondition{
+
+    private bool chooseWay;//关卡选择方向
+    private int RoadId;//道路ID
+    private float work;//事业值
+    private float family;//家庭值
+    private float status;//地位值
+    private float iniHeart;//已获得初心
+
+    public float getWork()
+    {
+        return work;
+    }
+    public float getFamily()
+    {
+        return family;
+    }
+    public float getStatus()
+    {
+        return status;
+    }
+    public void setWork(float value)
+    {
+        work = value;
+    }
+    public void setFamily(float value)
+    {
+        family = value;
+    }
+    public void setStatus(float value)
+    {
+        status = value;
+    }
+
+    public bool getChooseWay()
+    {
+        return chooseWay;
+    }
+    public void setChooseWay(bool choose)
+    {
+        chooseWay = choose;
+    }
+
+    public int getRoadId()
+    {
+        return RoadId;
+    }
+    public void setRoadId(int id)
+    {
+        RoadId = id;
+    }
+
+    public float getIniHeart()
+    {
+        return iniHeart;
+    }
+
+    public void setiniHeart(float value)
+    {
+        iniHeart += value;
+    }
+}
